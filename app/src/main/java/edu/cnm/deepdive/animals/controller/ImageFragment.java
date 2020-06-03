ae1d91f6cf039425fec8eeb84dfe9d1bcabdda72
Paul C <pcutter@cnm.edu>
2020-06-03 09:28:23 -0600

    Reformatted code

--- app/src/main/java/edu/cnm/deepdive/animals/controller/ImageFragment.java
+++ app/src/main/java/edu/cnm/deepdive/animals/controller/ImageFragment.java
@@ -25,75 +25,75 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ImageFragment extends Fragment {
 
-    private WebView contentView;
+  private WebView contentView;
+
+  @Override
+  public View onCreateView(LayoutInflater inflater, ViewGroup container,
+      Bundle savedInstanceState) {
+
+    View root = inflater.inflate(R.layout.fragment_image, container, false);
+    setupWebView(root);
+    return root;
+  }
+
+  private void setupWebView(View root) {
+    contentView = root.findViewById(R.id.content_view);
+    contentView.setWebViewClient(new WebViewClient() {
+      @Override
+      public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+        return false;
+      }
+    });
+    WebSettings settings = contentView.getSettings();
+    settings.setJavaScriptEnabled(true);
+    settings.setSupportZoom(true);
+    settings.setBuiltInZoomControls(true);
+    settings.setDisplayZoomControls(false);
+    settings.setUseWideViewPort(true);
+    settings.setLoadWithOverviewMode(true);
+    new Retriever().start();
+
+  }
+
+  private class Retriever extends Thread {
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-        Bundle savedInstanceState) {
+    public void run() {
 
-        View root = inflater.inflate(R.layout.fragment_image, container, false);
-        setupWebView(root);
-        return root;
-    }
+      Gson gson = new GsonBuilder()
+          .excludeFieldsWithoutExposeAnnotation()
+          .create();
+      Retrofit retrofit = new Retrofit.Builder()
+          .baseUrl(BuildConfig.BASE_URL)
+          .addConverterFactory(GsonConverterFactory.create(gson))
+          .build();
 
-    private void setupWebView(View root) {
-        contentView = root.findViewById(R.id.content_view);
-        contentView.setWebViewClient(new WebViewClient() {
+      AnimalService animalService = retrofit.create(AnimalService.class);
+
+      try {
+
+        Response<List<Animal>> response = animalService.getAnimals(BuildConfig.CLIENT_KEY)
+            .execute();
+        if (response.isSuccessful()) {
+          List<Animal> animals = response.body();
+          assert animals != null;
+          final String url = animals.get(0).getUrl();
+          getActivity().runOnUiThread(new Runnable() {
             @Override
-            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
-                return false;
+            public void run() {
+              contentView.loadUrl(url);
             }
-        });
-        WebSettings settings = contentView.getSettings();
-        settings.setJavaScriptEnabled(true);
-        settings.setSupportZoom(true);
-        settings.setBuiltInZoomControls(true);
-        settings.setDisplayZoomControls(false);
-        settings.setUseWideViewPort(true);
-        settings.setLoadWithOverviewMode(true);
-        new Retriever().start();
-
-    }
-
-    private class Retriever extends Thread {
-
-        @Override
-        public void run() {
-
-            Gson gson = new GsonBuilder()
-                .excludeFieldsWithoutExposeAnnotation()
-                .create();
-            Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl(BuildConfig.BASE_URL)
-                .addConverterFactory(GsonConverterFactory.create(gson))
-                .build();
-
-            AnimalService animalService = retrofit.create(AnimalService.class);
-
-            try {
-
-                Response<List<Animal>> response = animalService.getAnimals(BuildConfig.CLIENT_KEY)
-                    .execute();
-                if(response.isSuccessful()) {
-                    List<Animal> animals = response.body();
-                    assert animals != null;
-                    final String url = animals.get(0).getUrl();
-                    getActivity().runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            contentView.loadUrl(url);
-                        }
-                    });
-                } else {
-                    Log.e("AnimalService", response.message());
-                }
-
-
-            } catch (IOException e) {
-                Log.e("AnimalService", e.getMessage(), e);
-            }
-
+          });
+        } else {
+          Log.e("AnimalService", response.message());
         }
+
+
+      } catch (IOException e) {
+        Log.e("AnimalService", e.getMessage(), e);
+      }
+
     }
+  }
 
 }
